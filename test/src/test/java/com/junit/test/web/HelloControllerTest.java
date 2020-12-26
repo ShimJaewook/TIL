3688fbfefd67d5c6fb16716efc5610570b940c4a
ShimJaewook <ts4837829@naver.com>
2020-12-26 22:54:53 +0900

    Add Test JUnit src

---
+++ test/src/test/java/com/junit/test/web/HelloControllerTest.java
@@ -0,0 +1,16 @@
+package com.junit.test.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+
+
+
+}
